987f63779af855020fb6cc0fd796668d951602f3
omarelsheekh <31706237+omarelsheekh@users.noreply.github.com>
2019-07-08 13:25:11 +0200

    added docs

--- cashtable.py
+++ cashtable.py
@@ -5,20 +5,34 @@ dataArr=[]
 timeArr=[]
 boolArr=[]
 class MyThread(threading.Thread):
+    """this class inherit from threading.Thread class
+    it's important for creating threads which will expire the values
+    """
     def run(self):
-        #print("thread started")
+        """In this fun we will overwrite the one in the super class
+        """
         i= len(dataArr)-1
         time.sleep(timeArr[i])
         boolArr[i]=False
-        #print("an item has been expired")
 
 def addCash(v,t):
+    """Add cash values to the table.
+    Arguments:
+        v {str} -- the value which will be stored
+        t {int} -- the time after which the value will be expired
+    """
     dataArr.append(v)
     timeArr.append(t)
     boolArr.append(True)
     thr = MyThread()
     thr.start()
 def CheckCash(data):
+    """Check for the value if it still exist or expired or doesn't exist att all
+    Arguments:
+        data {str} -- the value you want to check
+    Returns:
+        str -- returns a string which told us if it still exist or expired or doesn't exist att all
+    """
     if data in dataArr:
         if boolArr[dataArr.index(data)]:
             return "item  exist "
